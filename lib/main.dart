62155dedce874fb2e2e5cf2790e302334ee6738b
FrederikvSvane <s224766@student.dtu.dk>
2023-06-06 15:29:44 +0200

    NavigationBar and five empty pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fitness_app/pages/home.dart';
+import 'package:flutter_fitness_app/pages/navigation.dart';
 
 void main() {
   runApp(MaterialApp(
+
+    // theme: ThemeData(
+    //   brightness: Brightness.light,
+    //   /* light theme settings */
+    // ),
+    // darkTheme: ThemeData(
+    //   brightness: Brightness.dark,
+    //   /* dark theme settings */
+    // ),
+    // themeMode: ThemeMode.dark,
+    // /* ThemeMode.system to follow system theme,
+    //      ThemeMode.light for light theme,
+    //      ThemeMode.dark for dark theme
+    //   */
+
+    debugShowCheckedModeBanner: false,
+
     initialRoute: "/",
     routes: {
-      "/": (context) => Home(),
+      "/": (context) => Navigation(),
     },
 
   ));
