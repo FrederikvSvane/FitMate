33048cbdec132c9c04eba02d8727e9d898ea407f
Martin-Surlykke <116875874+Martin-Surlykke@users.noreply.github.com>
2023-06-08 16:11:28 +0200

    New frontpage design

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -3,8 +3,21 @@ import 'package:flutter/material.dart';
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile Page"),
+    return Scaffold(
+      body: Container (
+        color: Colors.red[800],
+        child: Stack(
+          children: [
+            Positioned.fill(
+        child: Opacity (
+          opacity: 0.2,
+                child: Image.asset('assets/image/Front page 2.png',
+                fit: BoxFit.cover)
+        )
+            )
+          ],
+        )
+      )
     );
   }
 }
