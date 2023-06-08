9bca744b5d80cc261618ba4636ead019f44088f2
ssschoubye <116802763+ssschoubye@users.noreply.github.com>
2023-06-08 17:16:01 +0200

    Button added

--- lib/pages/workout.dart
+++ lib/pages/workout.dart
@@ -3,8 +3,29 @@ import 'package:flutter/material.dart';
 class Workout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Workout Page"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Workout Page'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Text("Workout Page"),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Button click logic goes here
+              },
+              child: Text('Start Workout'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
