fe680bb84d5814a0af2d3eec9e6a4edc8eadee12
Martin-Surlykke <116875874+Martin-Surlykke@users.noreply.github.com>
2023-06-09 13:36:13 +0200

    List included

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,7 +2,28 @@ import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
 
-  String userName = 'Jesper';
+  final List<String> items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8',
+    'Item 9',
+    'Item 10',
+    'Item 11',
+    'Item 12',
+    'Item 13',
+    'Item 14',
+    'Item 15',
+    'Item 16',
+    'Item 17',
+    'Item 18',
+    'Item 19',
+    'Item 20',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -89,16 +110,25 @@ class Profile extends StatelessWidget {
         ),
     )
     ),
-    Expanded(
+     Expanded(
     flex: 5,
-    child: Container(
-    color: Colors.white
+    child: Scaffold (
+      body: ListView.builder(
+        itemCount: items.length,
+          itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+          );
+          }
+      ),
+
     ),
     ),
           ],
     ),
     ),
     );
+
 
 
 }
