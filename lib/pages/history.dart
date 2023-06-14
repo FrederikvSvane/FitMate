574802fcef6b9a39ea150987e8e71a5aaa6b88c2
Martin-Surlykke <116875874+Martin-Surlykke@users.noreply.github.com>
2023-06-14 16:25:16 +0200

    Update history.dart

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -10,104 +10,129 @@ class History extends StatefulWidget {
   _HistoryState createState() => _HistoryState();
 }
 
-class _HistoryState extends State<History>{
+class _HistoryState extends State<History> {
   @override
   initState() {
     super.initState();
   }
 
-  
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter demo',
-        home: Scaffold(
-          body: Column(
-            children:  [
-          Container(
-            height: 200,
-            color: Colors.red[800],
-            padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
-            child: Stack(
-              children: [
-
-
-                Align(
-            alignment: Alignment.topRight,
-
-                child: IconButton(
-                  color: Colors.white,
-                  icon: const Icon(Icons.calendar_today),
-                  onPressed: () async {
-                    final DateTime? picked = await showDatePicker(
-                      context: context,
-                      initialDate: selectedDate,
-                      firstDate: DateTime(2023),
-                      lastDate: DateTime.now(),
-                    );
-                    if (picked != null && picked != selectedDate) {
-                      setState(() {
-                        selectedDate = picked;
-                      });
-                      setState(() {});
-                    }
-                  },
-                ),
-                ),
-
-                const Padding(
-                padding: EdgeInsets.only(top:15),
-                  child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    'Nutrition',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black
+      title: 'Flutter demo',
+      home: Scaffold(
+          body: Column(children: [
+            Container(
+              height: 200,
+              color: Colors.red[800],
+              padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      color: Colors.white,
+                      icon: const Icon(Icons.calendar_today),
+                      onPressed: () async {
+                        final DateTime? picked = await showDatePicker(
+                          context: context,
+                          initialDate: selectedDate,
+                          firstDate: DateTime(2023),
+                          lastDate: DateTime.now(),
+                        );
+                        if (picked != null && picked != selectedDate) {
+                          setState(() {
+                            selectedDate = picked;
+                          });
+                          setState(() {});
+                        }
+                      },
                     ),
                   ),
-                ),
-                ),
-
-                Align(
-                  alignment: Alignment.center,
-
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Text(
-                     "123",
-                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500),
-                ),
-
-                      Text(
-                      DateFormat('EEEE, MMMM d, y').format(selectedDate),
-                      style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        'Nutrition',
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                    ),
                   ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                          color: Colors.white,
+                          icon: const Icon(Icons.arrow_back_ios_new),
+                          onPressed: () async {},
+                        ),
+                        Text(
+                          DateFormat('EEEE, MMMM d, y').format(selectedDate),
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        IconButton(
+                          color: Colors.white,
+                          icon: const Icon(Icons.arrow_forward_ios),
+                          onPressed: () async {},
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text('Protein: 140 grams',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
 
-              const Text(
-                "123",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500),
+                            ),),
+                          Text('Calories: 2340',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)
+                          ),
+                        ],
+                      ))
+                ],
               ),
-              ],
             ),
-                ),
+            const Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text('123'),
+                        Text('123')
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text('123'),
+                        Text('123')
+                      ],
+                    ),
+
+                  ],
+                )
+            ),
           ],
-            )
-              ),
-            ]
-    )
-        )
+          )
+      ),
     );
   }
-
 }
